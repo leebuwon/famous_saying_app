1485fc9596308aa7cdba0990edf4947e2c84476a
bw1611 <bw1611@naver.com>
2023-03-02 09:25:14 +0900

    2단ê계 : 명언등록

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,12 +9,16 @@ public class Main {
         System.out.println("== 명언 앱 ==");
 
         while (true){
-            System.out.printf("명령 : ");
+            System.out.printf("명령) ");
             String cmd = sc.nextLine();
 
             if (cmd.equals("종료")){
                 System.out.println("종료 되었습니다.");
                 break;
+            }else if (cmd.equals("등록")){
+                System.out.printf("명언 : ");
+                String famousSaying = sc.nextLine();
+                System.out.println("작가 : 작자미상");
             }
         }
     }
